6506d3b99a78990fca76371f6177665fbc96c7a5
rxfix <panter2004@gmail.com>
2021-09-28 11:45:10 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect('accountant.db')
+    cursor = conn.cursor()
+
+    #     Создадим пользователя с user_id = 1000
+    cursor.execute("INSERT OR IGNORE INTO `users` (`user_id`) VALUES (?)", (1001,))
+
+    #     Считываем всех пользователей
+    users = cursor.execute("SELECT * FROM `users` ")
+    print(users.fetchall())
+
+    #     Подтверждаем изменения
+    conn.commit()
+
+except sqlite3.Error as error:
+    print('Error', error)
+
+finally:
+    if(conn):
+        conn.close()
